7f76237a1f119c915de972afff2626abe94b3b47
Gopi (GKS) <gopikrishnans1996@gmail.com>
2024-05-01 17:50:29 +0530

    moved to dubverse tts

--- core/ai.py
+++ core/ai.py
@@ -39,10 +39,7 @@ def respond_voice(encoded_img):
     response_text = respond(encoded_img)
     if response_text:
         audio_response = dubverse_tts(response_text)
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
-            audio_response.stream_to_file(f.name)
-            audio_response = f.read()
-            encoded_audio = base64.b64encode(audio_response).decode("utf-8")
+        encoded_audio = base64.b64encode(audio_response.content).decode("utf-8")
         return {"response": response_text, "audio": encoded_audio}
     else:
         return {}
